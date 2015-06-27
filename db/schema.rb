5b6b9f1131ef95c27bb862135b66777b47787594
Sho Kodama <madakodama.1go@gmail.com>
2015-06-28 03:19:02 +0900

    いろいろ追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150627063447) do
+ActiveRecord::Schema.define(version: 20150627172053) do
 
   create_table "restaurants", force: :cascade do |t|
     t.string   "name"
@@ -48,6 +48,8 @@ ActiveRecord::Schema.define(version: 20150627063447) do
     t.string   "last_sign_in_ip"
     t.datetime "created_at",                          null: false
     t.datetime "updated_at",                          null: false
+    t.string   "family_name"
+    t.string   "given_name"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true
