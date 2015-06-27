83b95781949d602a204453404a4d2c7561d792f7
Sho Kodama <madakodama.1go@gmail.com>
2015-06-27 16:28:16 +0900

    モデルにポイントが入った！ので、あとは平均点を出す

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -9,7 +9,9 @@ class ReviewsController < ApplicationController
     @review = Review.new(
       restaurant_id: params[:restaurant_id],
       user_id: current_user.id,
-      body: review_params["body"]
+      point: review_params["point"],
+      body: review_params["body"],
+
     )
     if @review.save
       redirect_to restaurant_url(@review.restaurant)
