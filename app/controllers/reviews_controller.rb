1cd3d3ddb1ff43555a0f444d906331f4a9b3e1ae
Sho Kodama <madakodama.1go@gmail.com>
2015-06-27 23:22:36 +0900

    編集・削除機能の実装

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -10,8 +10,7 @@ class ReviewsController < ApplicationController
       restaurant_id: params[:restaurant_id],
       user_id: current_user.id,
       point: review_params["point"],
-      body: review_params["body"],
-
+      body: review_params["body"]
     )
     if @review.save
       redirect_to restaurant_url(@review.restaurant)
@@ -19,6 +18,30 @@ class ReviewsController < ApplicationController
       render :new
     end
   end
+
+  def edit
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.find(params[:id])
+  end
+
+   def update
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.find(params[:id])
+    if @review.update(review_params)
+      redirect_to @restaurant
+    else
+      render :edit
+    end
+  end
+
+  def destroy
+    @review = Review.find(params[:id])
+    restaurant_id = @review.restaurant_id
+    @review.destroy
+    redirect_to restaurant_url(restaurant_id)
+  end
+
+
 
   private
 
