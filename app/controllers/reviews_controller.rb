2018517ef3905e2e5bf989f8cb2e6a82c1a3a561
Sho Kodama <madakodama.1go@gmail.com>
2015-06-27 16:13:44 +0900

    基本部分完成　点数計算

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,27 @@
 class ReviewsController < ApplicationController
+
+  def new
+    @restaurant = Restaurant.find(params[:restaurant_id])
+    @review = Review.new
+  end
+
+  def create
+    @review = Review.new(
+      restaurant_id: params[:restaurant_id],
+      user_id: current_user.id,
+      body: review_params["body"]
+    )
+    if @review.save
+      redirect_to restaurant_url(@review.restaurant)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:body)
+  end
+
 end
